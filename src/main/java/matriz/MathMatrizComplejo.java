b68c47e13e5c550199c329c8c7bac46613090cbc
camilosaenz <rubian.saenz@mail.escuelaing.edu.co>
2019-08-28 18:29:00 -0500

    Matrices Complejas

---
+++ src/main/java/matriz/MathMatrizComplejo.java
@@ -0,0 +1,60 @@
+package main.java.matriz;
+
+import main.java.complejos.Complejo;
+import main.java.complejos.MathComplejo;
+
+public class MathMatrizComplejo {
+	
+	private MathComplejo operaciones = new MathComplejo();
+	
+	/**
+	 * 
+	 */
+	public static Complejo[] sumaVector(Complejo[] vector1, Complejo[] vector2) {
+		
+		if(vector1.length == vector2.length) {
+			Complejo[] suma = new Complejo[vector1.length];
+			for (int i=0; i<vector1.length;i++) {
+				Complejo valor = MathComplejo.suma(vector1[i], vector2[i]);
+				suma[i] = valor;
+			}
+			return suma;
+			
+		}else {
+			return null;
+		}
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public static Complejo[] multiplicacionEscalar(Complejo[] vector1, Complejo num1) {
+		
+		Complejo[] multiplicacion = new Complejo[vector1.length];
+		for (int i=0; i<vector1.length; i++) {
+			Complejo valor = MathComplejo.producto(vector1[i], num1);
+			multiplicacion[i] = valor;
+		}
+		return multiplicacion;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public static MatrizComplejo sumaMatriz(MatrizComplejo matriz1, MatrizComplejo matriz2) {
+		if((matriz1.getColumna() == matriz2.getColumna()) && (matriz1.getFila() == matriz2.getFila())) {
+			MatrizComplejo sumaMatriz = new MatrizComplejo(matriz1.getFila(), matriz1.getColumna());
+			for(int i = 0; i < matriz1.getFila(); i++) {
+				for (int j = 0; j < matriz1.getColumna(); j++) {
+					sumaMatriz.getMatriz()[i][j] = MathComplejo.suma(matriz1.getMatriz()[i][j], matriz2.getMatriz()[i][j]);
+				}
+			}
+			return sumaMatriz;
+		}else {
+			return null;
+		}
+	}
+	
+}
---
+++ src/main/java/matriz/MatrizComplejo.java
@@ -0,0 +1,79 @@
+package main.java.matriz;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import main.java.complejos.*;
+
+public class MatrizComplejo {
+	private int fila;
+	private int columna;
+	private Complejo[][] matriz;
+	
+	
+	public MatrizComplejo(int fila, int columna) {
+		this.fila=fila;
+		this.columna=columna;
+	}
+
+
+	public int getFila() {
+		return fila;
+	}
+
+
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+
+
+	public int getColumna() {
+		return columna;
+	}
+
+
+	public void setColumna(int columna) {
+		this.columna = columna;
+	}
+
+
+	public Complejo[][] getMatriz() {
+		return matriz;
+	}
+
+
+	public void setMatriz(Complejo[][] matriz) {
+		this.matriz = matriz;
+	}
+	
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this==o) {
+			return true;
+		}
+		if(o==null || getClass()!=o.getClass()) {
+			return false;
+		}
+		MatrizComplejo that = (MatrizComplejo) o;
+		Boolean equalmatrix=true;
+		
+		if(fila==that.fila && columna==that.columna) {
+			for(int i=0; i<fila && fila==that.fila && columna==that.columna;i++) {
+				for(int j=0; j<columna;j++) {
+					equalmatrix=this.getMatriz()[i][j].equals(that.getMatriz()[i][j]);
+				}
+			}
+		}
+		return equalmatrix;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(fila,columna);
+		result = 31*result+Arrays.hashCode(matriz);
+		return result;
+		
+	}
+}
